40f6e3a0d0e7421daf6289acf6cd52c218f581fd
Bhavnish Narang <Bhavnish@fpgrowth.io>
2025-09-18 13:18:10 +0530

    Update webflow.js

--- api/webflow.js
+++ api/webflow.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
-    console.log("response", data);
+    console.log("RAW Webflow response:", data);
 
     let filtered = data.items;
 
